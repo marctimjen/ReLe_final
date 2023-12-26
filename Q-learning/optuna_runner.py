462f16845bebd9203d8411917bd5f6287fae98d5
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-12-26 12:41:40 +0100

    update

--- Q-learning/optuna_runner.py
+++ Q-learning/optuna_runner.py
@@ -20,10 +20,11 @@ params = {"direction": "minimize", "n_trials": 20}
 run["parameters"] = params
 
 optuna.delete_study(study_name="Q_learn_study", storage="sqlite:///example.db")
-study = optuna.create_study(storage="sqlite:///example.db", study_name="Q_learn_study")
+study = optuna.create_study(storage="sqlite:///example.db", study_name="Q_learn_study", direction="minimize")
 n_amount = 3
 
-commands = [[py_path + f" /home/tyson/ReLe_final/Q-learning/opt_test.py -r {run_id} -i {i}"] for i in range(n_amount)]
+commands = [[py_path, "/home/tyson/ReLe_final/Q-learning/opt_test.py", "-r", str(run_id), "-i", str(i)]
+            for i in range(n_amount)]
 
 for command in commands:
     p = subprocess.Popen(command, stdout=subprocess.PIPE)
